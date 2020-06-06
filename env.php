4bbe57733ee709461068fb88aa1b984331a34d86
carsaca <varsava-marko@gmail.com>
2020-06-06 21:06:44 +0200

    Prebacivanje na externu bazu

--- env.php
+++ env.php
@@ -4,8 +4,8 @@ namespace db;
 
 class DbConfig
 {
-    const HOST = "localhost";
-    const USER = "root";
-    const PASS = "";
-    const DB = "web_cfc";
+    const HOST = "eu-cdbr-west-03.cleardb.net";
+    const USER = "bd30954cc8a378";
+    const PASS = "ffe2d39a";
+    const DB = "heroku_73c29ebb10490cf";
 }
